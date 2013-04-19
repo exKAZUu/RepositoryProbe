4df0081f990d6af3c3194e0530ee0214569ea659
Ryohei Takasawa <henshinfukanou@gmail.com>
2013-04-19 15:08:49 +0900

    Create new class for using git and maven.

--- src/main/java/net/exkazuu/GitManager.java
+++ src/main/java/net/exkazuu/GitManager.java
@@ -1,6 +1,9 @@
 package net.exkazuu;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class GitManager {
 	private static GitManager gm;
@@ -17,7 +20,7 @@ public class GitManager {
 	}
 
 	public void clone(String address, String name) {
-		String command = "git clone " + address;
+		String command = ("git clone " + address + " C:/" + name);
 		try {
 			rt.exec(command);
 		} catch (IOException e) {
@@ -26,10 +29,25 @@ public class GitManager {
 		}
 	}
 	
-	public Boolean test() {
-		String command = "mvn test";
+	public Boolean test(String name) {
+		String command = "mvn test C:/" + name;
 		try {
-			rt.exec(command);
+			Process p =rt.exec(command);
+			InputStream is = p.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			
+			String res;
+			Boolean flag = false;
+			while((res = br.readLine()) != null) {
+				if(res.contains("SUCCESSFULL")) {
+					flag = true;
+				}
+			}
+			
+			if(flag) {
+				System.out.println(name);
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
