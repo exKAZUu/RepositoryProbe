0fd9e05f2c67d38d7f42f267e33e7a6162a82b74
Kazunori SAKAMOTO <exkazuu@gmail.com>
2013-07-10 15:44:31 +0900

    Add PomModifier project.

---
+++ PomModifier/PomModifier/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PomModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) {
+                Console.WriteLine("Please specify the path of the pom file.");
+            }
+            var xdoc = new XDocument(args[0]);
+
+        }
+    }
+}
